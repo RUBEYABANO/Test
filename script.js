37843de20d774351b271962aa0f33a1d2345e604
Rubeya Bano <rbano@blinkcharging.com>
2024-04-30 16:34:29 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const accountId = 12345
+let accountEmail= "bano@gmail.com"
+accountPassword= "1243"
+accountCity= "jaipur"
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
